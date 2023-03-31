bf12492948f4e5f2a79455956c6f8d0887a2a58d
Jan Tsizik <jtsizik@3-E-5.42heilbronn.de>
2023-03-31 15:21:38 +0200

    first attempt

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// @ts-ignore
-async function bootstrap() : Promise<void> {
-    return NestFactory.create(AppModule)
-      .then((app) => app.listen(6969))
-      .catch((err) => console.error(err));
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+		origin: true,
+		credentials: true,
+	});
+  await app.listen(3000);
 }
-
-bootstrap()
-  .then(() => console.log('Application started successfully.'))
-  .catch((err) => console.error(`Error starting application: ${err}`));
+bootstrap();
