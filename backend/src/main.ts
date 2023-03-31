e178a526c8e45c88878ee1023d1a5449ecaf381a
FVNRLS <rmazurit@student.42heilbronn.de>
2023-03-31 10:47:51 +0200

    docker intit script added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-  async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(6969);
-  }
-  bootstrap();
+// @ts-ignore
+async function bootstrap() : Promise<void> {
+    return NestFactory.create(AppModule)
+      .then((app) => app.listen(6969))
+      .catch((err) => console.error(err));
+}
+
+bootstrap()
+  .then(() => console.log('Application started successfully.'))
+  .catch((err) => console.error(`Error starting application: ${err}`));
