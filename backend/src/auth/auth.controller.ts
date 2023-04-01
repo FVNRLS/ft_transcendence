024011957fe3b8792f955d6ad69b8d5b1bf26e32
FVNRLS <rmazurit@student.42heilbronn.de>
2023-04-01 18:07:24 +0200

    auth struct extended

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service'
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+	constructor(private authService: AuthService) {
+
+	}
+
+	@Post('/local/signup')
+	signupLocal(@Body() dto: AuthDto) {
+		this.authService.signupLocal(dto);
+	}
+
+	@Post('/local/signin')
+	signinLocal() {
+		this.authService.signinLocal();
+	}
+
+	@Post('/logout')
+	logout() {
+		this.authService.logout();
+	}
+
+	@Post('/refresh')
+	refreshTokens() {
+		this.authService.refreshTokens();
+	}
+}
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+
+@Injectable()
+export class AuthService {
+	constructor(private prisma: PrismaService) {}
+
+	signupLocal(dto: AuthDto) {
+		// const newUser = this.prisma.user.create()
+	}
+
+	signinLocal() {
+
+	}
+
+	logout() {
+
+	}
+
+	refreshTokens() {
+
+	}
+}
