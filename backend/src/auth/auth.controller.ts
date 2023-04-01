b5542aac1aa00da7343add253c006e16a57d799b
FVNRLS <rmazurit@student.42heilbronn.de>
2023-04-01 12:16:36 +0200

    auth changed on front and backend

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,16 +1,18 @@
-//AUTH API
-
-import { Controller, Body, Header, Param, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
-
 export class AuthController {
+    constructor(private authService: AuthService) {}
 
-    constructor(private readonly AuthService: AuthService) {}
-
-    @Get()
-    getHelloWorld(): string {
-        return this.AuthService.getHelloWorld();
+    @Post()
+    async authenticate(@Body() body: { accessToken: string }) {
+        try {
+            // Use the access token to get the user information from 42 API
+            const user = await this.authService.getUser(body.accessToken);
+            console.log('Authenticated user:', user);
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 5000;
+
   app.enableCors({
 		origin: true,
 		credentials: true,
 	});
+
   await app.listen(PORT);
 }
+
 bootstrap();
