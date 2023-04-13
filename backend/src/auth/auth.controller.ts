0c180919acb6d70f23e8739846d24af6a6a27168
FVNRLS <rmazurit@student.42heilbronn.de>
2023-04-13 17:43:50 +0200

    google access token getter added

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -26,18 +26,18 @@ export class AuthController {
 
 	@Post('/upload')
 	@UseInterceptors(FileInterceptor('file', { dest: 'uploads' }))
-	async upload(@Body() dto: AuthDto, @UploadedFile() file: Express.Multer.File): Promise<{ status: HttpStatus, message?: string }> {
+	async uploadProfilePicture(@Body() dto: AuthDto, @UploadedFile() file: Express.Multer.File): Promise<{ status: HttpStatus, message?: string }> {
 		return this.authService.uploadProfilePicture(dto, file);
 	}
 
 	@Post('/delete')
 	@UseInterceptors(FileInterceptor('file', { dest: 'uploads' }))
-	async delete(@Body() dto: AuthDto): Promise<{ status: HttpStatus, message?: string }> {
+	async deleteProfilePicture(@Body() dto: AuthDto): Promise<{ status: HttpStatus, message?: string }> {
 		return this.authService.deleteProfilePicture(dto);
 	}
 
 	@Post('/get_google_drive_access_token')
-	async getToken(@Body() dto: AuthDto): Promise<{ status: HttpStatus, message?: string }> {
+	async getGoogleDriveAcessToken(@Body() dto: AuthDto): Promise<{ status: HttpStatus, message?: string }> {
 		return this.authService.getGoogleDriveAcessToken(dto);
 	}
 }
