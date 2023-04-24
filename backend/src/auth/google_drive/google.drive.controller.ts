376b9ab2713e44c8bbb4240384af79e5729383ac
FVNRLS <rmazurit@student.42heilbronn.de>
2023-04-24 12:53:35 +0200

    refactoring on google drive service started

--- backend/src/auth/google_drive/google.drive.controller.ts
+++ backend/src/auth/google_drive/google.drive.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpStatus, Post, UploadedFile, UseInterceptors, Req, Res } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthDto } from '../dto';
+import { ApiResponse } from '../dto/response.dto';
 import { GoogleDriveService } from './google.drive.service';
 
 @Controller('/storage')
@@ -11,23 +12,39 @@ export class GoogleDriveController {
 	
 	@Post('/upload')
 	@UseInterceptors(FileInterceptor('file', { dest: 'uploads' }))
-	async uploadProfilePicture(@Body('cookie') cookie: string, @UploadedFile() file: Express.Multer.File): Promise<{ status: HttpStatus, message?: string }> {
-		return this.googleDriveService.uploadProfilePicture(cookie, file);
+	async uploadProfilePicture(@Body('cookie') cookie: string, @UploadedFile() file: Express.Multer.File): Promise<ApiResponse> {
+		try {
+			return this.googleDriveService.uploadProfilePicture(cookie, file);
+		} catch (error) {
+			return error;
+		}
 	}
 
 	@Post('/delete')
 	// @UseInterceptors(FileInterceptor('file', { dest: 'uploads' }))
-	async deleteProfilePicture(@Body('cookie') cookie: string): Promise<{ status: HttpStatus, message?: string }> {
-		return this.googleDriveService.deleteProfilePicture(cookie);
+	async deleteProfilePicture(@Body('cookie') cookie: string): Promise<ApiResponse> {
+		try {
+			return this.googleDriveService.deleteProfilePicture(cookie);
+		} catch (error) {
+			return error;
+		}
 	}
 
 	@Post('/get_google_drive_access_token')
-	async getGoogleDriveAcessToken(@Body() dto: AuthDto): Promise<{ status: HttpStatus, message?: string }> {
-		return this.googleDriveService.getGoogleDriveAcessToken(dto);
+	async getGoogleDriveAcessToken(@Body() dto: AuthDto): Promise<ApiResponse> {
+		try {
+			return this.googleDriveService.getGoogleDriveAcessToken(dto);
+		} catch (error) {
+			return error;
+		}
 	}
 
 	@Post('/get_profile_picture')
-	async getProfilePicture(@Body() dto: AuthDto): Promise<{ fieldname: string; originalname: string; encoding: string; mimetype: string; buffer: any; size: number; }> | null {
-		return this.googleDriveService.getProfilePicture(dto);
+	async getProfilePicture(@Body() dto: AuthDto): Promise<{ fieldname: string; originalname: string; encoding: string; mimetype: string; buffer: any; size: number; }> {
+		try {
+			return this.googleDriveService.getProfilePicture(dto);
+		} catch (error) {
+			return error;
+		}
 	}
 }
