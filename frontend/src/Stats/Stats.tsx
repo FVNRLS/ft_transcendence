c7bea25d88f7d8b392e1594410cc76ff303dd865
Jan Tsizik <jtsizik@3-E-5.42heilbronn.de>
2023-06-17 13:57:05 +0200

    game stats window

--- frontend/src/Stats/Stats.tsx
+++ frontend/src/Stats/Stats.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 interface GameScore {
 	enemyName: String,
@@ -19,6 +21,7 @@ const Stats = () => {
 	const navigate = useNavigate();
 
 	const [scoreArr, setScoreArr] = useState<GameScore[]>([]);
+	const [isRating, setIsRating] = useState(false);
 
 	useEffect(() => {
 		if (!session)
@@ -46,8 +49,15 @@ const Stats = () => {
 		<>
 			<Header />
 			<div className="bg">
+				{ !isRating && (
+				<>
 				<div className="stats-cont">
 				<div className="table-header">Game Statistics</div>
+					{!scoreArr[0] && (
+					<div className="table-row">
+						<div className="column">No Stats Yet</div>
+					</div>
+					)}
 					{scoreArr.map((score) => (
 					<div className="table-row">
 						<div className="column">{score.enemyName}</div>
@@ -55,6 +65,11 @@ const Stats = () => {
 					</div>
 					))}
 				</div>
+				<FontAwesomeIcon className="icon-right" 
+					icon={faArrowRight} color="#f9f9f9" size="2x" onClick={() => {setIsRating(true)}}/>
+				</>)}
+				{isRating && <FontAwesomeIcon className="icon-left"
+					icon={faArrowLeft} color="#f9f9f9" size="2x" onClick={() => {setIsRating(false)}}/>}
 			</div>
 		</>
 	)
